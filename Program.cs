a6c7edf90679d8ecb45abf98f30024eeea2c7931
GiorgosDrivas <78447254+GiorgosDrivas@users.noreply.github.com>
2022-04-17 17:57:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+{
+        int Number = 4;
+        int PlayerNum;
+        int m;
+
+        Console.WriteLine("Pick a number beetween 1 and 4");
+        Console.WriteLine("Write 1 if you want to quit");
+        m = Convert.ToInt32(Console.ReadLine());
+
+        switch (m)
+        {
+            case 1:
+                Environment.Exit(0);
+                break;
+            case 2:
+                do
+                    {
+                        Console.WriteLine("Write a number!");
+                        PlayerNum = Convert.ToInt32(Console.ReadLine());
+                        if(PlayerNum == Number)
+                        {
+                            Console.WriteLine("Congrats! You found the number!");
+                        } 
+                        else
+                            Console.WriteLine("Wrong!Try again.");
+                    }
+                    while(PlayerNum != Number);
+                    break;
+            default:
+            break;
+        }
+        
+}       
+    }
+}
+
+
+    
